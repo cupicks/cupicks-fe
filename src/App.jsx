c28f778e090956a91651244e632cdefc7c5d242e
wonseok-do <wsdo970@gmail.com>
2022-08-29 16:15:45 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 const App = () => {
-<<<<<<< HEAD
   return <div className="App"></div>;
 };
-=======
-  return (
-    <div className="App">
-      <Router />
-    </div>
-  )
-}
->>>>>>> 965a5a10328bb3725f83c4bf36c046b58a7216c6
 
 export default App;
