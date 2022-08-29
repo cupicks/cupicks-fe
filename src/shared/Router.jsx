bc97337025015af377ca7e4be80eeb416308a6a5
Dusuna <dusunax@gmail.com>
2022-08-29 23:39:57 +0900

    update Router.jsx 머지 충돌 해결
    pull 받아주세요!

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import Login from "../pages/Login";
-import RecipeCreate from "../pages/RecipeCreate";
 import Recipe from "../pages/Recipe";
+import RecipeCreate from "../pages/RecipeCreate";
 import RecipeDetail from "../pages/RecipeDetail";
 import Register from "../pages/register";
 
@@ -10,11 +10,11 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/recipe" element={<Recipe />} />
         <Route path="/recipe/create" element={<RecipeCreate />} />
         <Route path="/recipe/detail/:recipeId" element={<RecipeDetail />} />
         <Route path="/signIn" element={<Login />} />
         <Route path="/signUp" element={<Register />} />
-        <Route path="/recipe" element={<Recipe />} />
       </Routes>
     </BrowserRouter>
   );
