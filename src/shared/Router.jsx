cd9dfe0fa256aae381ea0146c4a65363a7ca4463
wonseok-do <wsdo970@gmail.com>
2022-08-29 23:09:11 +0900

    create 로그인,회원가입 (페이지, 라우터) 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,16 +1,20 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom"
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "../pages/Login";
 import RecipeCreate from "../pages/RecipeCreate";
 import RecipeDetail from "../pages/RecipeDetail";
+import Register from "../pages/register";
 
 const Router = () => {
-  return (  
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/recipe/create" element={<RecipeCreate />}/>
-        <Route path="/recipe/detail/:recipeId" element={<RecipeDetail />}/>
+        <Route path="/recipe/create" element={<RecipeCreate />} />
+        <Route path="/recipe/detail/:recipeId" element={<RecipeDetail />} />
+        <Route path="/signIn" element={<Login />} />
+        <Route path="/signUp" element={<Register />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default Router;
