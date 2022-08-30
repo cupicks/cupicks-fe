8d886faf2c03dfae134a2682a6f19c743ef5eaa6
cmun2 <cmun2@illinois.edu>
2022-08-30 23:05:47 +0900

    create 프로필 수정 페이지 컴포넌트

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,19 +2,21 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import Login from "../pages/Login";
 import Recipe from "../pages/Recipe";
-import RecipeCreate from "../pages/RecipeCreate";
+// import RecipeCreate from "../pages/RecipeCreate";
 import RecipeDetail from "../pages/RecipeDetail";
 import Register from "../pages/register";
+import RegisterEdit from "../pages/RegisterEdit";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/recipe" element={<Recipe />} />
-        <Route path="/recipe/create" element={<RecipeCreate />} />
+        {/* <Route path="/recipe/create" element={<RecipeCreate />} /> */}
         <Route path="/recipe/detail/:recipeId" element={<RecipeDetail />} />
         <Route path="/signIn" element={<Login />} />
         <Route path="/signUp" element={<Register />} />
+        <Route path="/signedit/:userId" element={<RegisterEdit />} />
       </Routes>
     </BrowserRouter>
   );
