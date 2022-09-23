46c00e92ab419273e7eb7097983603cd0abff68f
Dusuna <dusunax@gmail.com>
2022-09-24 00:41:26 +0900

    Feature 레시피 삭제 버튼 구현

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 import api from "../server/api";
 
@@ -8,16 +8,57 @@ import IngredientsContainer from "../components/recipeDetail/IngredientsContaine
 import RecipeDesc from "../components/recipeDetail/RecipeDesc";
 import Navigation from "../partial/Navigation";
 import ToastMessage from "../components/elements/modal/ToastMessage";
+import ConfirmBox from "../components/elements/modal/ConfirmBox";
 
 import styled from "styled-components";
 
 const RecipeDetail = () => {
   // location state로 메시지 모달창 출력
   const location = useLocation();
+  const { recipeId } = useParams();
+  const [recipe, setRecipe] = useState("");
+  const navigate = useNavigate();
+
+  const [showConfirmBox, setShowConfirmBox] = useState(false);
   const [messageModal, setMessageModal] = useState(false);
+
   const messageText = location.state?.message;
   const timer = 1800;
 
+  /**************************/
+  /** 레시피 삭제 버튼 핸들러 */
+  const recipeDeleteButtonClickHandler = () => {
+    setShowConfirmBox(true);
+  };
+
+  const recipeDeleteConfirm = () => {
+    setShowConfirmBox(false);
+    recipeDeleting();
+  };
+
+  const recipeDeleteDenied = () => {
+    setShowConfirmBox(false);
+  };
+
+  const confirmProps = {
+    recipeDeleteButtonClickHandler,
+    recipeDeleteConfirm,
+    recipeDeleteDenied,
+  };
+
+  const recipeDeleting = async () => {
+    let contentType = "application/json";
+
+    try {
+      const response = await api(contentType).delete(`/recipes/${recipeId}`);
+      navigate("/recipe", { state: { message: "메시지가\n삭제 되었습니다." } });
+    } catch (err) {
+      console.log(err);
+      navigate("/404");
+    }
+  };
+
+  // umount시 메시지 체크
   useEffect(() => {
     if (messageText !== undefined) {
       setMessageModal(true);
@@ -27,10 +68,7 @@ const RecipeDetail = () => {
     }
   }, []);
 
-  const { recipeId } = useParams();
-  const [recipe, setRecipe] = useState("");
-
-  const Recipefetching = async () => {
+  const recipefetching = async () => {
     let contentType = "application/json";
 
     try {
@@ -39,11 +77,12 @@ const RecipeDetail = () => {
       console.log(response.data);
     } catch (err) {
       console.log(err);
+      navigate("/404");
     }
   };
 
   useEffect(() => {
-    Recipefetching();
+    recipefetching();
   }, []);
 
   return (
@@ -57,11 +96,20 @@ const RecipeDetail = () => {
           </Navigation>
 
           <IngredientsContainer recipe={recipe} />
-          <RecipeDesc recipe={recipe} />
+          <RecipeDesc recipe={recipe} confirmProps={confirmProps} />
         </>
       )}
 
       {messageModal && <ToastMessage text={messageText} timer={timer} />}
+
+      {showConfirmBox && (
+        <ConfirmBox
+          text={"레시피를 삭제하시겠습니까"}
+          confirmButtonText={"삭제합니다."}
+          onComfirmed={recipeDeleteConfirm}
+          onDenied={recipeDeleteDenied}
+        />
+      )}
     </StWrap>
   );
 };
