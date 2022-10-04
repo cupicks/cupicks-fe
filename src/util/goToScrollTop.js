850055ab3c8c36465c812250668c27d76ca13f54
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2022-10-05 06:26:06 +0900

    Feature 탑 버튼 추가 및 프로필 수정 페이지 CSS (#326)
    * #298 회원가입페이지 오류 수정 및 desc 추가
    
    * Update SEO 기본 최적화 (#321)
    
    * Update 레시피 상세 페이지 SEO
    
    * Update 댓글 alt메시지
    
    * Update 레시피 수정 페이지 autoComplete off, 레시피 key값
    
    * Update 댓글 이미지 랜더링 버그 수정
    
    * update 프로필 수정 페이지 뒤로가기 버튼 수정
    
    * Feature 커뮤니티 페이지에 탑버튼 추가 (#323)
    
    * Feature 커뮤니티 페이지에 탑버튼 추가
    
    * Update NotFound 페이지에 img 기본 높이 추가
    
    * Update 레시피상세 페이지 수정,삭제 버튼 추가
    
    * Style 프로필 수정 페이지 CustomStyle적용 (#325)
    
    * Feature 커뮤니티 페이지에 탑버튼 추가
    
    * Update NotFound 페이지에 img 기본 높이 추가
    
    * Style 프로필 수정 페이지 CustomStyle적용
    
    Co-authored-by: wonseok-do <wsdo970@gmail.com>
    Co-authored-by: wonseok-do <108975442+wonseok-do@users.noreply.github.com>

--- src/util/goToScrollTop.js
+++ src/util/goToScrollTop.js
@@ -1,14 +1,16 @@
 /**
  * scrollTopToRef은 스크롤이 이동할 위치의 ref,
  * scrollElement은 스크롤이 일어나는 컴포넌트 영역의 ref입니다.
- * @param {ref} scrollTopToRef
  * @param {ref} scrollElement
+ * @param {ref} scrollTopToRef
  */
-const gotoScrollTop = (scrollTopToRef, scrollElement) => {
-  const targetScrollTop = scrollTopToRef.current.offsetTop;
-
+const gotoScrollTop = (scrollElement, scrollTopToRef) => {
+  let targetScrollTop = 0;
+  if (scrollTopToRef) {
+    targetScrollTop = scrollTopToRef.current.offsetTop + 10;
+  }
   scrollElement.current.scrollTo({
-    top: targetScrollTop + 10,
+    top: targetScrollTop,
     behavior: "smooth",
   });
 };
