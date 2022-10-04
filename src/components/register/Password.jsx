92ba0d9cb793d14c0fe49190215c87674e649a95
wonseok-do <wsdo970@gmail.com>
2022-10-05 04:23:10 +0900

    #298 회원가입페이지 오류 수정 및 desc 추가

--- src/components/register/Password.jsx
+++ src/components/register/Password.jsx
@@ -25,10 +25,10 @@ const Password = (props) => {
   return (
     <StDiv>
       {passwordError && (
-        <ToastMessage text={errors?.password?.message} timer={1000} />
+        <ToastMessage text={errors?.password?.message} timer={1800} />
       )}
       {passwordCfError && (
-        <ToastMessage text={"비밀번호가 일치하지 않습니다."} timer={1000} />
+        <ToastMessage text={"비밀번호가 일치하지 않습니다."} timer={1800} />
       )}
       <CustomTitle>
         <h1>비밀번호 입력</h1>
