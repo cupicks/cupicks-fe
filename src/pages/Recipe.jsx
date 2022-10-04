719aa756872ecb1a91ebbc2217f8f0a1ddb583bd
Dusuna <dusunax@gmail.com>
2022-10-05 05:29:57 +0900

    Feature 커뮤니티 페이지에 탑버튼 추가

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
 import styled from "styled-components";
 import styledLayoutComponents from "../styles/customLayoutStyle";
 const { CustomWrapNoHeader } = styledLayoutComponents;
@@ -10,24 +6,35 @@ import RecipeBody from "../components/recipeMain/RecipeBody";
 import UserGuide from "../components/userGuide/UserGuide";
 import { useRef } from "react";
 
+import { gotoScrollTop as gotoScrollTop } from "../util/goToScrollTop";
+
+import topButton from "../assets/svg/top_button.svg";
+
 const Recipe = (props) => {
   const { loggedIn } = props;
   const scrollTopLookAround = useRef();
   const scrollElement = useRef();
 
   return (
-    <StWrapBody ref={scrollElement}>
-      <UserGuide
-        loggedIn={loggedIn}
-        scrollTopLookAround={scrollTopLookAround}
-        scrollElement={scrollElement}
-      />
-      <div ref={scrollTopLookAround} />
+    <>
+      <StWrapBody ref={scrollElement}>
+        <UserGuide
+          loggedIn={loggedIn}
+          scrollTopLookAround={scrollTopLookAround}
+          scrollElement={scrollElement}
+        />
+        <div ref={scrollTopLookAround} />
 
-      <RecipeBody loggedIn={loggedIn} />
+        <RecipeBody loggedIn={loggedIn} />
+      </StWrapBody>
 
-      <button>hi</button>
-    </StWrapBody>
+      <StTopButton
+        className="top_button"
+        onClick={() => gotoScrollTop(scrollElement)}
+      >
+        <img src={topButton} alt="맨 위로 가기" />
+      </StTopButton>
+    </>
   );
 };
 
@@ -43,4 +50,24 @@ const StWrapBody = styled(CustomWrapNoHeader)`
   }
 
   overflow: auto;
+`;
+
+const StTopButton = styled.button`
+  position: absolute;
+  bottom: 10rem;
+  right: 3rem;
+
+  padding: 0.5rem;
+
+  opacity: 0.8;
+  transition: all 0.2s;
+
+  :hover {
+    opacity: 1;
+    transform: translateY(-0.5rem);
+  }
+
+  img {
+    width: 3.6rem;
+  }
 `;
